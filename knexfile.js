aa728ae46d1d3224efb7992beffcf089da66ff32
A Đỗ <adork2410@gmail.com>
2022-10-08 20:45:01 +0700

    push

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
     connection: {
         connectionString: process.env.DB_URL,
         ssl: {
-            require: true,
+            sslmode: "require",
             rejectUnauthorized: false
           }
     },
