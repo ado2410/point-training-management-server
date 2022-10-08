ce0167de003e4b363e02cc0d1717f8367bfafbda
A Đỗ <adork2410@gmail.com>
2022-10-08 12:49:13 +0700

    push

--- knexfile.js
+++ knexfile.js
@@ -1,18 +1,16 @@
 require("dotenv").config();
 
 module.exports = {
-    development: {
-        client: "pg",
-        connection: {
-            connectionString: process.env.DB_URL,
-            ssl: false,
-        },
-        searchPath: ["knex", "public"],
-        migrations: {
-            directory: "./db/migrations",
-        },
-        seeds: {
-            directory: "./db/seeds",
-        },
+    client: "pg",
+    connection: {
+        connectionString: process.env.DB_URL,
+        ssl: false,
+    },
+    searchPath: ["knex", "public"],
+    migrations: {
+        directory: "./db/migrations",
+    },
+    seeds: {
+        directory: "./db/seeds",
     },
 };
